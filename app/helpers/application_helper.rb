8971f44dc359d7debe5ef5c61b1c0eb876e478fa
danteata <dan.abakah@gmail.com>
2011-06-19 18:36:39 +0000

    made the title slightly dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+ def title
+   if @title.nil?
+     "Ruby on Rails Tutorial Sample App"
+   else
+
+     "Ruby on Rails Tutorial Sample App | #{@title}"
+   end
+ end
 end
+
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have right display title" do
+      get 'home'
+      response.should have_selector("title", 
+		     :content => "Ruby")
     end
   end
 
@@ -16,4 +23,10 @@ describe PagesController do
     end
   end
 
+ describe "GET 'about'" do
+   it "should be successful" do
+	get 'about'
+	response.should be_success
+   end
+ end
 end
